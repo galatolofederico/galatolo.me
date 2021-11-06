c54ce0774a3df2348bcc5a51638ecae7a2c24113
Federico Galatolo <galatolo.federico@gmail.com>
2021-11-06 16:52:52 +0000

    added cloud domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(galatolo.me static.galatolo.me pubs.galatolo.me git.galatolo.me api.galatolo.me)
+domains=(galatolo.me static.galatolo.me pubs.galatolo.me git.galatolo.me api.galatolo.me cloud.galatolo.me)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="galatolo.federico@gmail.com"
